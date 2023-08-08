bb385604a358ffce5bdc250b92211479d72cc20f
Tharindu Ishan <ishanthari96@gmail.com>
2023-08-08 09:15:25 +0530

    2nd commit

--- Demo1.java
+++ Demo1.java
@@ -14,65 +14,65 @@ public class Demo1{
         double subMarks3=76;
         double avg;
 
-        // Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         
-        // System.out.print("Enter your Name :");
-        // String name=scanner.nextLine().strip();
-        // if(name.length()==0 || name.isEmpty()) System.out.println("\u001B[31mInvalid Name");
+        System.out.print("Enter your Name :");
+        String name=scanner.nextLine().strip();
+        if(name.length()==0 || name.isEmpty()) System.out.println("\u001B[31mInvalid Name");
         
-        // else {
-        //     studentName=name;
-        //     System.out.print("Enter your Age: ");
-        //     String age1=scanner.nextLine();
-        //     int age=Integer.parseInt(age1);
+        else {
+            studentName=name;
+            System.out.print("Enter your Age: ");
+            String age1=scanner.nextLine();
+            int age=Integer.parseInt(age1);
             
-        //     if(age<10||age>18) System.out.println("\u001B[31mInvalid Age");
-        //     else{
-        //         studentAge=age;
-        //         System.out.print("Enter your subject 1: ");
-        //         String sub1=scanner.nextLine();
-        //         if(!sub1.startsWith("SE-")) System.out.println("\u001B[31mInvalid Subject 1");
-        //         else{
-        //             subName1=sub1;
-        //             System.out.printf("Enter your marks 1: ");
-        //             String subM1x=scanner.nextLine();
-        //             double subM1=Double.parseDouble(subM1x);
-        //             if(subM1<0||subM1>100) System.out.println("\u001B[31mInvalid Marks 1");
-        //             else{
-        //                 subMarks1=subM1;
-        //                 System.out.print("Enter your subject 2: ");
-        //                 String sub2=scanner.nextLine();
-        //                 if(!sub2.startsWith("SE-")||sub2.equals(sub1)) System.out.println("\u001B[31mInvalid Subject 2");
-        //                 else{
-        //                     subName2=sub2;
-        //                     System.out.printf("Enter your marks 2: ");
-        //                     String subM2x=scanner.nextLine();
-        //                     double subM2=Double.parseDouble(subM2x);
-        //                     if(subM2<0||subM2>100) System.out.println("\u001B[31mInvalid Marks 2");
-        //                     else{
-        //                         subMarks2=subM2;
-        //                         System.out.print("Enter your subject 3: ");
-        //                         String sub3=scanner.nextLine();
-        //                         if(!sub3.startsWith("SE-")||sub3.equals(sub1)||sub3.equals(sub2)) System.out.println("\u001B[31mInvalid Subject 3");
-        //                         else{
-        //                             subName3=sub3;
-        //                             System.out.printf("Enter your marks 3: ");
-        //                             String subM3x=scanner.nextLine();
-        //                             double subM3=Double.parseDouble(subM3x);
-        //                             if(subM3<0||subM3>100) System.out.println("\u001B[31mInvalid Marks 3");
-        //                             else subMarks3=subM3;
+            if(age<10||age>18) System.out.println("\u001B[31mInvalid Age");
+            else{
+                studentAge=age;
+                System.out.print("Enter your subject 1: ");
+                String sub1=scanner.nextLine();
+                if(!sub1.startsWith("SE-")) System.out.println("\u001B[31mInvalid Subject 1");
+                else{
+                    subName1=sub1;
+                    System.out.printf("Enter your marks 1: ");
+                    String subM1x=scanner.nextLine();
+                    double subM1=Double.parseDouble(subM1x);
+                    if(subM1<0||subM1>100) System.out.println("\u001B[31mInvalid Marks 1");
+                    else{
+                        subMarks1=subM1;
+                        System.out.print("Enter your subject 2: ");
+                        String sub2=scanner.nextLine();
+                        if(!sub2.startsWith("SE-")||sub2.equals(sub1)) System.out.println("\u001B[31mInvalid Subject 2");
+                        else{
+                            subName2=sub2;
+                            System.out.printf("Enter your marks 2: ");
+                            String subM2x=scanner.nextLine();
+                            double subM2=Double.parseDouble(subM2x);
+                            if(subM2<0||subM2>100) System.out.println("\u001B[31mInvalid Marks 2");
+                            else{
+                                subMarks2=subM2;
+                                System.out.print("Enter your subject 3: ");
+                                String sub3=scanner.nextLine();
+                                if(!sub3.startsWith("SE-")||sub3.equals(sub1)||sub3.equals(sub2)) System.out.println("\u001B[31mInvalid Subject 3");
+                                else{
+                                    subName3=sub3;
+                                    System.out.printf("Enter your marks 3: ");
+                                    String subM3x=scanner.nextLine();
+                                    double subM3=Double.parseDouble(subM3x);
+                                    if(subM3<0||subM3>100) System.out.println("\u001B[31mInvalid Marks 3");
+                                    else subMarks3=subM3;
            
            
-        //                             }
-        //                         }
+                                    }
+                                }
 
-        //                     }
-        //                 }
+                            }
+                        }
 
-        //             }
+                    }
 
-        //         }
-        //     }
+                }
+            }
         System.out.println(studentName+" "+studentAge+" "+subName1+" "+subName2+" "+subName3+" "+subMarks1+" "+subMarks2+" "+subMarks3);
         avg=(subMarks1+subMarks2+subMarks3)/3;
         int s1=Integer.parseInt(subName1.substring(3));
